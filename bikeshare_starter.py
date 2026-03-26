b9251d3c93f46888e32864046bc7917ffe666a3b
Rajneet Kaur-Reyat <rajneet.kaur-reyat@cariad.technology>
2026-03-26 15:25:53 +0100

    Test print statement is added

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -47,7 +47,7 @@ def load_data(city, month, day):
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
-
+    print('This new line is added for testing')
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
